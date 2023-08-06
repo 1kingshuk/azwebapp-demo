63fdfc21ae246dabc1c6ab12c8bab963a6e09e78
Kingshuk Ghosh <Kingshuk.Ghosh1@gds.ey.com>
2023-08-06 20:13:08 +0530

    Added Maven project

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hi, Welcome to Azure World!");
+        System.out.println("Hi, Welcome to Azure!");
     }
 }
